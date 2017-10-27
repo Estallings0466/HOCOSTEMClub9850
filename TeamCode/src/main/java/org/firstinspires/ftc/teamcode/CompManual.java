fb45543514990c5d01a2c58e5c4431b75dc6e054
rpf_B <brennen.robotics0413@gmail.com>
2017-10-26 21:57:12 -1000

    Created new mecanum manual program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompManual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompManual.java
@@ -1,17 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
-
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
+        import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+        import com.qualcomm.robotcore.hardware.DcMotor;
+        import com.qualcomm.robotcore.hardware.Servo;
+        import com.qualcomm.robotcore.util.Range;
 
 
 /*
-   Holonomic concepts from:
-   http://www.vexforum.com/index.php/12370-holonomic-drives-2-0-a-video-tutorial-by-cody/0
+	Holonomic concepts from:
+	http://www.vexforum.com/index.php/12370-holonomic-drives-2-0-a-video-tutorial-by-cody/0
    Robot wheel mapping:
           X FRONT X
         X           X
@@ -23,27 +21,30 @@ import com.qualcomm.robotcore.util.Range;
         X           X
           X       X
 */
-@TeleOp(name = "New Main Manual", group = "Mcanum Program")
+@TeleOp(name = "Comp Main Manual", group = "Main Manual MAC")
 //@Disabled
-public class ProMecanum1 extends OpMode {
+public class CompManual extends OpMode {
 
-    DcMotor Relic_Arm;
-    DcMotor Pivot;
-    //Servo Jewel_Arm;
-    //Servo Left_Claw;
-   // Servo Right_Claw;
+    DcMotor Front_Right;
     DcMotor Front_Left;
     DcMotor Back_Right;
-    DcMotor Front_Right;
     DcMotor Back_Left;
-     Servo Relic_Claw;
-    //DcMotor Glyph_lift;
-    // Servo SwivelClaw;
+    DcMotor Relic_Arm;
+    DcMotor Pivot;
+    DcMotor Glyph_lift;
+    Servo Relic_Claw;
+    Servo Left_Claw;
+    Servo Right_Claw;
+    Servo Right_ClawBot;
+    Servo Left_ClawBot;
+
+    Servo LJewel_Arm;
+    Servo RJewel_Arm;
 
     /**
      * Constructor
      */
-    public ProMecanum1() {
+    public CompManual() {
 
     }
 
@@ -51,34 +52,35 @@ public class ProMecanum1 extends OpMode {
     public void init() {
 
 
-      /*
-       * Use the hardwareMap to get the dc motors and servos by name. Note
-       * that the names of the devices must match the names used when you
-       * configured your robot and created the configuration file.
-       */
-         Relic_Arm = hardwareMap.dcMotor.get("RA");
-         Relic_Claw = hardwareMap.servo.get("REC");
-        Pivot = hardwareMap.dcMotor.get("P");
-       // Left_Claw = hardwareMap.servo.get("LC");
-       // Right_Claw = hardwareMap.servo.get("RC");
-        Front_Left = hardwareMap.dcMotor.get("FL");
-        Back_Right = hardwareMap.dcMotor.get("BR");
+		/*
+		 * Use the hardwareMap to get the dc motors and servos by name. Note
+		 * that the names of the devices must match the names used when you
+		 * configured your robot and created the configuration file.
+		 */
+
+
         Front_Right = hardwareMap.dcMotor.get("FR");
-        Back_Left = hardwareMap.dcMotor.get("BL ");
-      //  Glyph_lift = hardwareMap.dcMotor.get("GL");
-        //Jewel_Arm = hardwareMap.servo.get("JA");
-        //SwivelClaw = hardwareMap.servo.get("swivel_claw");
+        Front_Left = hardwareMap.dcMotor.get("FL");
+        Back_Left = hardwareMap.dcMotor.get("BL");
+        Back_Right = hardwareMap.dcMotor.get("BR");
+        Relic_Arm = hardwareMap.dcMotor.get("RA");
+        Pivot = hardwareMap.dcMotor.get("P");
+        Relic_Claw = hardwareMap.servo.get("REC");
+        Glyph_lift = hardwareMap.dcMotor.get("GL");
+        Left_Claw = hardwareMap.servo.get("LC");
+        Left_ClawBot = hardwareMap.servo.get("LCB");
+         Right_Claw = hardwareMap.servo.get("RC");
+        Right_ClawBot = hardwareMap.servo.get("RCB");
+
+        LJewel_Arm = hardwareMap.servo.get("LJA");
+       RJewel_Arm = hardwareMap.servo.get("RJA");
+
         //These work without reversing (Tetrix motors).
         //AndyMark motors may be opposite, in which case uncomment these lines:
         //motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
         //motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
         //motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
         //motorBackRight.setDirection(DcMotor.Direction.REVERSE);
-        //Left_Claw.setDirection(Servo.Direction.REVERSE);
-       // Right_Claw.setDirection(Servo.Direction.REVERSE);
-      //  Glyph_lift.setDirection(DcMotorSimple.Direction.REVERSE);
-        //Jewel_Arm.setDirection(Servo.Direction.FORWARD);
-        //reverse lifts autonomous arm up
 
     }
 
@@ -89,18 +91,17 @@ public class ProMecanum1 extends OpMode {
         // left stick controls direction
         // right stick X controls rotation
 
-        float gamepad1LeftY = gamepad1.left_stick_y;
+        float gamepad1LeftY = -gamepad1.left_stick_y;
         float gamepad1LeftX = gamepad1.left_stick_x;
         float gamepad1RightX = gamepad1.right_stick_x;
-        float Lclaw = gamepad2.left_trigger;
-        float Rclaw = gamepad2.left_trigger;
-          float Claw = gamepad2.right_trigger;
-          float Arm = gamepad2.left_stick_y;
+        float Claw = gamepad2.right_trigger;
+        float Arm = gamepad2.left_stick_y;
         float P = gamepad2.right_stick_x;
+        float Lclaw = gamepad2.left_trigger;
+        float LBClaw = gamepad2.left_trigger;
+        float Rclaw = gamepad2.left_trigger;
+        float RBClaw = gamepad2.left_trigger;
         float Slide = gamepad2.right_stick_y;
-
-        // float Swivel = gamepad2.left_trigger;
-
 
         // holonomic formulas
 
@@ -116,27 +117,36 @@ public class ProMecanum1 extends OpMode {
         BackRight = Range.clip(BackRight, -1, 1);
 
         // write the values to the motors
-        Front_Left.setPower(FrontLeft);
-        Back_Right.setPower(BackRight);
         Front_Right.setPower(FrontRight);
+        Front_Left.setPower(FrontLeft);
         Back_Left.setPower(BackLeft);
-       // Left_Claw.setPosition(Lclaw);
-       // Right_Claw.setPosition(Rclaw);
-        Relic_Arm.setPower(Arm);
+        Back_Right.setPower(BackRight);
         Relic_Claw.setPosition(Claw);
         Pivot.setPower(P);
-      //  Glyph_lift.setPower(Slide);
-        //  SwivelClaw.setPosition(Swivel);
-       // Right_Claw.setDirection(Servo.Direction.FORWARD);
+        Relic_Arm.setPower(Arm);
+        Left_Claw.setPosition(Lclaw);
+        Left_ClawBot.setPosition(LBClaw);
+         Right_Claw.setPosition(Rclaw);
+        Right_ClawBot.setPosition(RBClaw);
+        Glyph_lift.setPower(Slide);
+
+        Left_Claw.setDirection(Servo.Direction.REVERSE);
+        Left_ClawBot.setDirection(Servo.Direction.REVERSE);
+         Right_Claw.setDirection(Servo.Direction.REVERSE);
+        Right_ClawBot.setDirection(Servo.Direction.REVERSE);
+        Glyph_lift.setDirection(DcMotor.Direction.REVERSE);
+
+        LJewel_Arm.setDirection(Servo.Direction.FORWARD);
+        RJewel_Arm.setDirection(Servo.Direction.FORWARD);
 
 
-      /*
-       * Telemetry for debugging
-       */
+		/*
+		 * Telemetry for debugging
+		 */
         telemetry.addData("Text", "*** Robot Data***");
-        telemetry.addData("Joy XL YL XR", String.format("%.2f", gamepad1LeftX) + " " +
-                String.format("%.2f", gamepad1LeftY) + " " + String.format("%.2f", gamepad1RightX));
-        telemetry.addData("f left pwr", "front left  pwr: " + String.format("%.2f", FrontLeft));
+        telemetry.addData("Joy XL YL XR",  String.format("%.2f", gamepad1LeftX) + " " +
+                String.format("%.2f", gamepad1LeftY) + " " +  String.format("%.2f", gamepad1RightX));
+        telemetry.addData("f left pwr",  "front left  pwr: " + String.format("%.2f", FrontLeft));
         telemetry.addData("f right pwr", "front right pwr: " + String.format("%.2f", FrontRight));
         telemetry.addData("b right pwr", "back right pwr: " + String.format("%.2f", BackRight));
         telemetry.addData("b left pwr", "back left pwr: " + String.format("%.2f", BackLeft));
@@ -153,9 +163,9 @@ public class ProMecanum1 extends OpMode {
      * scaled value is less than linear.  This is to make it easier to drive
      * the robot more precisely at slower speeds.
      */
-    double scaleInput(double dVal) {
-        double[] scaleArray = {0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
-                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00};
+    double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
 
         // get the corresponding index for the scaleInput array.
         int index = (int) (dVal * 16.0);
@@ -181,4 +191,5 @@ public class ProMecanum1 extends OpMode {
         // return scaled value.
         return dScale;
     }
+
 }
